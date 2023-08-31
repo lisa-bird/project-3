c2335e6a6f714677a97603abba63f4f7d0980053
lisa-bird <>
2023-08-31 09:37:04 +0000

    added changes for deployment

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
+import os
 from flaskr import create_app
+if os.path.exists("env.py"):
+    import env
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run( )
+    app.run( 
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+    ) 
+    
+    
+    
